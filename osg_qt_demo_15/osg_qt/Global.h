4c1362bc24d55dd0fdaa86559d97da72eeff66a3
bigPC <bigPC@example.com>
2016-01-13 22:45:51 +0800

    add missing Global.h file

---
+++ osg_qt_demo_15/osg_qt/Global.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <string>
+using namespace std;
+struct MyCompRule{
+	bool operator() (const string& l, const string& r) const{
+		const char *lchar = l.c_str();
+		const char *rchar = r.c_str();
+		int i = 0, lval = 0, rval = 0;
+		while(lchar[i] != '\0'){
+			lval += lchar[i];
+			i++;
+		}
+
+		i = 0;
+		while(rchar[i] != '\0'){
+			rval += rchar[i];
+			i++;
+		}
+		if (lval > rval) return true;
+		else return false;
+    }
+};
