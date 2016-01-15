72b439683df0085895acf1f8b49d12827af6aa7e
bigPC <bigPC@example.com>
2016-01-15 21:07:28 +0800

    lovely textpanel, now the panel is easy to use

--- osg_qt_demo_15/osg_qt/TextPanel.cpp
+++ osg_qt_demo_15/osg_qt/TextPanel.cpp
@@ -1,6 +1,11 @@
 #include "TextPanel.h"
 
+/*
+textpanel(MatrixTransform)-geode-geom
+			                    -content
 
+in default draw textpanel on YZ plain
+*/
 TextPanel::TextPanel(void):osg::MatrixTransform()
 {
 	content = new osgText::Text;
@@ -8,10 +13,6 @@ TextPanel::TextPanel(void):osg::MatrixTransform()
 	geode = new osg::Geode();
 	content->setDataVariance(osg::Object::DYNAMIC); 
 	this->addChild(geode);
-	timeString = "";
-	textString = "床前明月光；\n疑是地上霜；\n举头望明月；\n低头思故乡；\n";
-	//width = 1.0;
-	//height = 1.0;
 }
 
 
@@ -19,46 +20,18 @@ TextPanel::~TextPanel(void)
 {
 }
 
-void TextPanel::addYZContent(const osg::Vec3& leftupCorner, 
-							 float width, float height, 
-							 float size, bool minus, 
+void TextPanel::addContent(float width, float height, float size,  
 							 string fontpath){
 	osg::Vec3Array* nodeList = new osg::Vec3Array();
-	nodeList->push_back(leftupCorner);
-	nodeList->push_back(leftupCorner + osg::Vec3(0.0f, width, 0.0f));
-	nodeList->push_back(leftupCorner + osg::Vec3(0.0f, width, height));
-	nodeList->push_back(leftupCorner + osg::Vec3(0.0f, 0.0f, height));
-
-	font = osgText::readFontFile(fontpath);
-	addContent(leftupCorner + osg::Vec3(1.0f, 0.0f, height-size*0.3), nodeList,  osgText::Text::YZ_PLANE, width, height,size, minus);
-	//set margin to up border, and 1 offset from plain
-}
-
-void TextPanel::addXZContent(const osg::Vec3& leftupCorner, 
-							 float width, float height, 
-							 float size, bool minus, 
-							 string fontpath){
-	osg::Vec3Array* nodeList = new osg::Vec3Array();
-	nodeList->push_back(leftupCorner);
-	nodeList->push_back(leftupCorner + osg::Vec3(width, 0.0f, 0.0f));
-	nodeList->push_back(leftupCorner + osg::Vec3(width,0.0f, height));
-	nodeList->push_back(leftupCorner + osg::Vec3(0.0f, 0.0f, height));
-
-	font = osgText::readFontFile(fontpath);
-	addContent(leftupCorner + osg::Vec3(0.0f, -1.0f, height-size*0.3), nodeList, osgText::Text::XZ_PLANE, width, height,  size, minus);
-	//set margin to up border, and 1 offset from plain
-}
-
-void TextPanel::addContent(const osg::Vec3& leftupCorner, 
-						   osg::Vec3Array* nodeList, 
-						   osgText::TextBase::AxisAlignment align, 
-						   float& width, float& height, 
-						   float& size, bool minus){
-	osg::ref_ptr<osg::Geometry> geom = new osg::Geometry();
+	nodeList->push_back(osg::Vec3(0, 0, 0));//make zero point on the left top
+	nodeList->push_back(osg::Vec3(0.0f, width, 0.0f));
+	nodeList->push_back(osg::Vec3(0.0f, width, height*(-1)));
+	nodeList->push_back(osg::Vec3(0.0f, 0.0f, height*(-1)));
 
 	osg::ref_ptr<osg::Vec4Array> colors = new osg::Vec4Array;
 	colors->push_back(osg::Vec4(0.4, 0.8, 0.4, 0.6));
 
+	osg::ref_ptr<osg::Geometry> geom = new osg::Geometry();
 	geom->addPrimitiveSet(new osg::DrawArrays(osg::PrimitiveSet::QUADS, 0, 4));
 	geom->setVertexArray(nodeList);
 	geom->setColorArray(colors);
@@ -67,22 +40,23 @@ void TextPanel::addContent(const osg::Vec3& leftupCorner,
 	stateset->setMode(GL_BLEND, osg::StateAttribute::ON);
 	geode->addDrawable(geom.get());
 
-	
-	setupProperties(*content, font, align, size, leftupCorner);
+	font = osgText::readFontFile(fontpath);
+	setupProperties(*content, font, size, osg::Vec3(0.2, size*0.5, (-1)*size*0.5));
 
 	geode->addDrawable(content.get());
-	if(minus == false)
-		this->setMatrix(osg::Matrix::rotate(3.141592657, osg::Vec3(0, 0, 1)));
 }
 
-//void TextPanel::updateContent(string cont){
-//	cont = cont+textString;
-//	int requiredSize = mbstowcs(NULL, cont.c_str(), 0);
-//	wchar_t * wtext = new wchar_t[requiredSize+1];
-//	mbstowcs(wtext , cont.c_str(), requiredSize+1);
-//	content->setText(wtext);
-//	delete wtext;
-//}
+void TextPanel::setupProperties(osgText::Text& textObject,
+								osgText::Font* font, 
+								float size,const osg::Vec3& offset ){
+   	textObject.setFont(font);
+	textObject.setCharacterSize(size);//字体大小
+	textObject.setPosition(offset);
+	textObject.setColor(osg::Vec4(0.8, 1.0, 0.9, 1.0));
+	textObject.setAlignment(osgText::Text::LEFT_TOP);
+	textObject.setAutoRotateToScreen(true);//跟随视角不断变化，但离物体越远，文字越小，和现实当中像类似
+	textObject.setAxisAlignment(osgText::Text::YZ_PLANE);
+}
 
 void TextPanel::updateContent(map<string,string, MyCompRule>* dbmap){
 	string contentString = "";
@@ -106,20 +80,4 @@ void TextPanel::updateContent(map<string,string, MyCompRule>* dbmap){
 	mbstowcs(wtext , contentString.c_str(), requiredSize+1);
 	content->setText(wtext);
 	delete wtext;
-}
-
-void TextPanel::setupProperties(osgText::Text& textObject,
-								osgText::Font* font, 
-								osgText::TextBase::AxisAlignment align, 
-								float size,const osg::Vec3& pos ){
-   	textObject.setFont(font);//
-	textObject.setCharacterSize(size);//字体大小
-	textObject.setPosition(pos);
-	//textObject.setRotation(osg::Quat(osg::PI_2, osg::Vec3(0, 1, 0)));
-	textObject.setColor(osg::Vec4(0.8, 1.0, 0.9, 1.0));
-	//textObject.setAlignment(osgText::Text::LEFT_BOTTOM);//文字显示方向
-	textObject.setAlignment(osgText::Text::LEFT_TOP);
-	//textObject.setAxisAlignment(osgText::Text::SCREEN);//获取文字对称成方式正对屏幕方向
-	textObject.setAutoRotateToScreen(true);//跟随视角不断变化，但离物体越远，文字越小，和现实当中像类似
-   textObject.setAxisAlignment(align);//获取文字对称成方式
 }
